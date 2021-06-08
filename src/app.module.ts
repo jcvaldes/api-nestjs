a1843a94cf38d6324030ea711216c17d2b0893a4
Juan Carlos Valdés <idevkingos@gmail.com>
2021-06-08 15:16:49 -0300

    incremento de producto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomersModule } from './customers/customers.module';
 
-
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://cencosud:swordfish@cluster0.k2q6w.mongodb.net/cencosud?retryWrites=true&w=majority',
+      'mongodb+srv://xxx:xxx@cluster0.k2q6w.mongodb.net/xxx?retryWrites=true&w=majority',
       // 'mongodb+srv://cencosud:swordfish@cluster0.wjkuq.mongodb.net/spid35?retryWrites=true&w=majority',
-      { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true}
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      },
     ),
-    CustomersModule
+    CustomersModule,
   ],
   controllers: [],
   providers: [],
